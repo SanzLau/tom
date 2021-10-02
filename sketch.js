0952ad7b5e9b3ac58c72b160467ab26d55a1a3bd
SanzLau <85967982+SanzLau@users.noreply.github.com>
2021-10-02 14:26:41 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+
+function preload() {
+//carga aquí las imágenes
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+//vcrea aquí los sprites de Tom y Jerry
+
+}
+
+function draw() {
+
+    background(bg);
+
+//escribe aquí la condición para evaluar se Tom y Jerry chocan
+    drawSprites();
+}
+
+
+function keyPressed(){
+//Escribe aquí el código para la animación de movimiento y cambio
+}
